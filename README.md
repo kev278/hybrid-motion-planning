2fe4423c1bda3ff16ea95e811a50868c73cd193b
Aakash Rohra <arohra@wpi.edu>
2022-04-19 22:42:09 -0400

    added robust planner

--- README.md
+++ README.md
@@ -19,6 +19,25 @@ dependency for prius
 ## Running the simulation
 
 ```sh 
-roslaunch turtlebot3_gazebo  turtlebot3_house.launch```
+roslaunch robust_planner  turtlebot3_world.launch```
+
+To see example of planning: 
+s
+rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped "header:
+  seq: 0
+  stamp:
+    secs: 0
+    nsecs: 0
+  frame_id: 'base_footprint'
+pose:
+  position:
+    x: 1
+    y: 0
+    z: 0.0
+  orientation:
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1" 
 
 
---
+++ robust_planner/include/robust_planner.h
@@ -0,0 +1,40 @@
+/** include the libraries you need in your planner here */
+/** for robust path planner interface */
+#include <ros/ros.h>
+#include <costmap_2d/costmap_2d_ros.h>
+#include <costmap_2d/costmap_2d.h>
+#include <nav_core/base_global_planner.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <angles/angles.h>
+#include <base_local_planner/world_model.h>
+#include <base_local_planner/costmap_model.h>
+
+using std::string;
+
+#ifndef GLOBAL_PLANNER_CPP
+#define GLOBAL_PLANNER_CPP
+
+namespace robust_planner
+{
+
+    class RobustPlanner : public nav_core::BaseGlobalPlanner
+    {
+    public:
+        RobustPlanner();
+        RobustPlanner(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+
+        /** overridden classes from interface nav_core::BaseGlobalPlanner **/
+        void initialize(std::string name, costmap_2d::Costmap2DROS *costmap_ros);
+        bool makePlan(const geometry_msgs::PoseStamped &start,
+                      const geometry_msgs::PoseStamped &goal,
+                      std::vector<geometry_msgs::PoseStamped> &plan);
+
+    private:
+        costmap_2d::Costmap2DROS *costmap_ros_;
+        double step_size_, min_dist_from_robot_;
+        costmap_2d::Costmap2D *costmap_;
+        base_local_planner::WorldModel *world_model_; ///< @brief The world model that the controller will use
+        bool initialized_;
+    };
+};
+#endif
